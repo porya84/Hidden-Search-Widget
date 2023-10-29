f415ff0d7bb2f209a9b63511b708489d2217a1e4
porya84 <76752078+porya84@users.noreply.github.com>
2023-10-29 19:49:19 +0330

    set input show event

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
 let searchInputElem = document.querySelector(".search_input");
 let btn = document.querySelector(".btn");
 let Show_flag = true;
+
+btn.addEventListener("click", () => {
+    if (Show_flag === true) {
+      console.log("a");
+      searchInputElem.classList.add("show");
+      Show_flag = false;
+    } else {
+      console.log("b");
+      searchInputElem.classList.remove("show");
+      Show_flag = true;
+    }
+  });
